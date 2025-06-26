a58d6a8bb0ef1b5ce852205a2a5ce2e42ca91ec7
Moa22men <moamenabdelrahman22@gmail.com>
2025-06-26 04:37:23 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            Console.Write("Enter a number: ");
+            int Num = int.Parse(Console.ReadLine());
+            if (Num % 3 == 0 && Num % 4 == 0)
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+            #endregion
+        }
+    }
+}
