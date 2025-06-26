54a274f712b3b3f5637e6b8224063b9af8d8cd8a
Moa22men <moamenabdelrahman22@gmail.com>
2025-06-26 05:20:59 +0300

    Q11 solved

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -114,6 +114,37 @@ namespace ConsoleApp3
             Console.WriteLine("Average Marks = " + average);
             Console.WriteLine("Percentage = " + percentage);
             #endregion
+
+            #region Q11
+            Console.Write("Enter month number: ");
+            int month = int.Parse(Console.ReadLine());
+            int days;
+            switch (month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    days = 31;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    days = 30;
+                    break;
+                case 2:
+                    days = 28;
+                    break;
+                default:
+                    days = 0;
+                    break;
+            }
+            Console.WriteLine("Days in Month: " + days);
+            #endregion
         }
     }
 }
